7b215cfebda9742e715b1123a200c1a053789bde
gSpot <gspot.x2f@gmail.com>
2021-01-08 18:04:05 +0300

    CamelCase

--- htdocs/luci-static/resources/view/syslog.js
+++ htdocs/luci-static/resources/view/syslog.js
@@ -3,10 +3,10 @@
 'require ui';
 
 return L.view.extend({
-	tail_default: 25,
-	pattern_default: '^',
+	tailDefault: 25,
+	patternDefault: '^',
 
-	get_log_data: function(last, pattern) {
+	getLogData: function(last, pattern) {
 		return Promise.all([
 			L.resolveDefault(fs.stat('/sbin/logread'), null),
 			L.resolveDefault(fs.stat('/usr/sbin/logread'), null),
@@ -23,48 +23,48 @@ return L.view.extend({
 	},
 
 	load: function() {
-		return this.get_log_data(this.tail_default, this.pattern_default);
+		return this.getLogData(this.tailDefault, this.patternDefault);
 	},
 
 	render: function(logdata) {
-		let nav_btns_top = '120px';
+		let navBtnsTop = '120px';
 
-		let log_textarea = E('textarea', {
+		let logTextarea = E('textarea', {
 			'id': 'syslog',
 			'class': 'cbi-input-textarea',
 			'style': 'width:100% !important; resize:horizontal; padding: 0 0 0 45px; font-size:12px',
 			'readonly': 'readonly',
 			'wrap': 'off',
-			'rows': this.tail_default,
+			'rows': this.tailDefault,
 			'spellcheck': 'false',
 		}, [ logdata.trim() ]);
 
-		let tail_value = E('input', {
-			'id': 'tail_value',
-			'name': 'tail_value',
+		let tailValue = E('input', {
+			'id': 'tailValue',
+			'name': 'tailValue',
 			'type': 'text',
-			'form': 'log_form',
+			'form': 'logForm',
 			'class': 'cbi-input-text',
 			'style': 'width:4em !important; min-width:4em !important',
 			'maxlength': 5,
 		});
-		tail_value.value = this.tail_default;
-		ui.addValidator(tail_value, 'uinteger', true);
+		tailValue.value = this.tailDefault;
+		ui.addValidator(tailValue, 'uinteger', true);
 
-		let log_filter = E('input', {
-			'id': 'log_filter',
-			'name': 'log_filter',
+		let logFilter = E('input', {
+			'id': 'logFilter',
+			'name': 'logFilter',
 			'type': 'text',
-			'form': 'log_form',
+			'form': 'logForm',
 			'class': 'cbi-input-text',
 			'style': 'margin-left:1em !important; width:16em !important; min-width:16em !important',
 			'placeholder': _('Message filter'),
 			'data-tooltip': _('Filter messages with a regexp'),
 		});
 
-		let log_form_submit_btn = E('input', {
+		let logFormSubmitBtn = E('input', {
 			'type': 'submit',
-			'form': 'log_form',
+			'form': 'logForm',
 			'class': 'cbi-button btn',
 			'style': 'margin-left:1em !important; vertical-align:middle',
 			'value': _('Apply'),
@@ -72,50 +72,50 @@ return L.view.extend({
 		});
 
 		return E([
-			E('h2', { 'id': 'log_title', 'class': 'fade-in' }, _('System Log')),
+			E('h2', { 'id': 'logTitle', 'class': 'fade-in' }, _('System Log')),
 			E('div', { 'class': 'cbi-section-descr fade-in' }),
 			E('div', { 'class': 'cbi-section fade-in' },
 				E('div', { 'class': 'cbi-section-node' },
-					E('div', { 'id': 'content_syslog', 'class': 'cbi-value' }, [
-						E('label', { 'class': 'cbi-value-title', 'for': 'tail_value' },
+					E('div', { 'id': 'contentSyslog', 'class': 'cbi-value' }, [
+						E('label', { 'class': 'cbi-value-title', 'for': 'tailValue' },
 							_('Show only the last messages')),
 						E('div', { 'class': 'cbi-value-field' }, [
-							tail_value,
+							tailValue,
 							E('input', {
 								'type': 'button',
 								'class': 'cbi-button btn cbi-button-reset',
 								'value': 'Χ',
 								'click': ev => {
-									tail_value.value = null;
-									log_form_submit_btn.click();
+									tailValue.value = null;
+									logFormSubmitBtn.click();
 									ev.target.blur();
 								},
 
 							}),
-							log_filter,
+							logFilter,
 							E('input', {
 								'type': 'button',
 								'class': 'cbi-button btn cbi-button-reset',
 								'value': 'Χ',
 								'click': ev => {
-									log_filter.value = null;
-									log_form_submit_btn.click();
+									logFilter.value = null;
+									logFormSubmitBtn.click();
 									ev.target.blur();
 								},
 							}),
-							log_form_submit_btn,
+							logFormSubmitBtn,
 							E('form', {
-								'id': 'log_form',
-								'name': 'log_form',
+								'id': 'logForm',
+								'name': 'logForm',
 								'style': 'display:inline-block; margin-left:1em !important',
 								'submit': ui.createHandlerFn(this, function(ev) {
 									ev.preventDefault();
-									let form_elems = Array.from(document.forms.log_form.elements);
-									form_elems.forEach(e => e.disabled = true);
+									let formElems = Array.from(document.forms.logForm.elements);
+									formElems.forEach(e => e.disabled = true);
 
-									return this.get_log_data(
-										(tail_value.value && tail_value.value > 0) ? tail_value.value : 0,
-										log_filter.value || this.pattern_default
+									return this.getLogData(
+										(tailValue.value && tailValue.value > 0) ? tailValue.value : 0,
+										logFilter.value || this.patternDefault
 									).then(logdata => {
 										logdata = logdata || '';
 										let loglines = logdata.match(/\n/g) || [];
@@ -124,11 +124,11 @@ return L.view.extend({
 											logdata = _('No matches...');
 										};
 
-										log_textarea.rows = (loglines.length < this.tail_default) ?
-											this.tail_default : loglines.length;
-										log_textarea.value = logdata.trim();
+										logTextarea.rows = (loglines.length < this.tailDefault) ?
+											this.tailDefault : loglines.length;
+										logTextarea.value = logdata.trim();
 									}).finally(() => {
-										form_elems.forEach(e => e.disabled = false);
+										formElems.forEach(e => e.disabled = false);
 									});
 								}),
 							}, E('span', {}, '&#160;')),
@@ -143,23 +143,23 @@ return L.view.extend({
 							E('button', {
 								'class': 'btn',
 								'style': 'position:relative; display:block; margin:0 !important; left:1px; top:'
-									+ nav_btns_top,
+									+ navBtnsTop,
 								'click': ev => {
-									document.getElementById('log_title').scrollIntoView(true);
+									document.getElementById('logTitle').scrollIntoView(true);
 									ev.target.blur();
 								},
 							}, '&#8593;'),
 							E('button', {
 								'class': 'btn',
 								'style': 'position:relative; display:block; margin:0 !important; margin-top:1px !important; left:1px; top:'
-									+ nav_btns_top,
+									+ navBtnsTop,
 								'click': ev => {
-									log_textarea.scrollIntoView(false);
+									logTextarea.scrollIntoView(false);
 									ev.target.blur();
 								},
 							}, '&#8595;'),
 						]),
-						log_textarea,
+						logTextarea,
 					])
 				)
 			),
