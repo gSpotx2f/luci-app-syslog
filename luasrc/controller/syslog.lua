6789afb9d52260edca3053313b512cef4b40fb0d
gSpot <gspot.x2f@gmail.com>
2021-12-03 22:23:18 +0300

    Moving packages

--- luasrc/controller/syslog.lua
+++ luasrc/controller/syslog.lua
@@ -2,5 +2,5 @@
 module('luci.controller.syslog', package.seeall)
 
 function index()
-    entry({'admin', 'services', 'syslog'}, view('syslog'), _('System Log'), 20).acl_depends = { 'luci-app-syslog' }
+	entry({'admin', 'services', 'syslog'}, view('syslog'), _('System Log'), 20).acl_depends = { 'luci-app-syslog' }
 end
